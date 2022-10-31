0df19f1e1f0f68af3a8013438baae8ae9b82567f
Mohamad Al Debs <mhd.debs@goldenpay.app>
2022-10-31 12:20:53 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Responsive Personal Expenses App
+A Personal Expenses App to help you track your spent money.
+
+<table>
+  <tr>
+    <td><img src="/screens_shots/1.png" width=270 height=480></td>
+    <td><img src="/screens_shots/2.png" width=270 height=480></td>
+    <td><img src="/screens_shots/3.png" width=270 height=480></td>
+  </tr>
+</table>
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:personal_expenses/pages/home_page.dart';
+
+
+void main() {
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitUp,
+  // ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Expenses',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+                labelMedium: TextStyle(color: Colors.white),
+              ),
+          primaryColor: Colors.purple,
+          appBarTheme: AppBarTheme(
+            toolbarTextStyle: ThemeData.light()
+                .textTheme
+                .copyWith(
+                  titleLarge: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+                .bodyText2,
+            titleTextStyle: ThemeData.light()
+                .textTheme
+                .copyWith(
+                  titleLarge: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+                .headline6,
+          ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(secondary: Colors.amber)),
+      home: HomePage(),
+    );
+  }
+}
